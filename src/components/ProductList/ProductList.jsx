161f8e9d7b45fcc509d1ffa88eed5f8cff6c53b8
alexisis111 <shuragilyow@yandex.ru>
2024-04-14 10:32:31 +0300

    init

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,18 +5,16 @@ import 'leaflet/dist/leaflet.css';
 const MapComponent = () => {
     const mapRef = useRef(null);
     const [userLocation, setUserLocation] = useState(null);
-    const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
     useEffect(() => {
         const map = mapRef.current;
 
-        // Проверяем, есть ли уже доступ к геолокации
-        if (!hasLocationPermission && "geolocation" in navigator) {
+        // Получаем текущее местоположение пользователя с помощью HTML5 Geolocation API
+        if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(
                 position => {
                     const { latitude, longitude } = position.coords;
                     setUserLocation([latitude, longitude]);
-                    setHasLocationPermission(true); // Устанавливаем флаг разрешения на геолокацию
                     if (map) {
                         map.setView([latitude, longitude], map.getZoom());
                     }
@@ -25,11 +23,8 @@ const MapComponent = () => {
                     console.error('Error getting user location:', error);
                 }
             );
-        } else if (userLocation && map) {
-            // Используем старое разрешение, если оно уже есть
-            map.setView(userLocation, map.getZoom());
         }
-    }, [mapRef, userLocation, hasLocationPermission]);
+    }, [mapRef]);
 
     return (
         <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '400px' }} ref={mapRef}>
